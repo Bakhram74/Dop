3305e260cf83417bfedc56017d15755c08c19ea8
Bakhram <abdurakhimov93@list.ru>
2022-10-19 08:27:40 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type AppPropsType =
+    {
+        userId: number
+        id: number
+        title: string
+        completed: boolean
+    }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState<AppPropsType[]>([])
+    const onClickHandler = () => {
+setTodos([])
+    }
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    },[])
+    return (
+        <div className="App">
+            <button onClick={onClickHandler}>CLEN POSTS</button>
+
+            <ul>
+                {todos.map(t => {
+                    return (
+                        <li>{`${t.userId} ${t.id} ${t.title} ${t.completed}`}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
